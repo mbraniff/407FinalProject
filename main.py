0594fbb44a61ace037867de13446e12050e9febe
mbraniff <mbraniff@rocketmail.com>
2021-04-17 12:20:32 -0500

    Idk What Im doing

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import numpy as np
+from PIL import Image
+from sklearn.decomposition import PCA
+
 def main():
-    print("Hello")
+    img = Image.open('train/cat.0.jpg').convert('LA')
+    numpydata = np.asarray(img)
+    x = len(numpydata)
+    y = len(numpydata[0])
+    mean = 0
+    for i in range(x):
+        for j in range(y):
+            mean += numpydata[i][j][0]
+    mean /= len(numpydata)*len(numpydata[0])
+    arr = []
+    for i in range(x):
+        arr.append([])
+        for j in range(y):
+            grey = numpydata[i][j][0] - mean
+            arr[i].append(grey)
+    arr = np.array(arr)
+    covMatrix = arr.dot(arr.transpose())
+    eigen = np.linalg.eig(covMatrix)
+    print(eigen)
     return 0
 
 
