2a3d473c9a45efdb3b69d3e7ec3b63bdb1711108
mbraniff <mbraniff@rocketmail.com>
2021-04-22 10:33:50 -0500

    Corrected CovMatrix

--- main.py
+++ main.py
@@ -1,35 +1,52 @@
 import numpy as np
 from PIL import Image
 
-def main():
-    img = Image.open('train/cat.0.jpg').convert('L')
-    img = img.resize((250,250))
-    img.show()
+pixX = 250
+pixY = 250
+
+setSize = 100
+
+def img_mean(img, mean):
     numpydata = np.asarray(img)
-    x = len(numpydata)
-    y = len(numpydata[0])
-    mean = 0
-    for i in range(x):
-        for j in range(y):
-            mean += numpydata[i][j]
-    mean /= len(numpydata)*len(numpydata[0])
-    arr = []
-    for i in range(x):
-        arr.append([])
-        for j in range(y):
-            grey = numpydata[i][j] - mean
-            arr[i].append(grey)
-    arr = np.array(arr)
-    covMatrix = arr.dot(arr.transpose())
+    mean = np.add(mean, numpydata)
+    return mean
+
+
+def main():
+    mean = np.zeros((pixX,pixY))
+
+    #Getting mean
+    for i in range(setSize):
+        name = 'train/cat.' + str(i) + '.jpg'
+        img = Image.open(name).convert('L')
+        img = img.resize((pixX,pixY))
+        mean = img_mean(img, mean)
+    mean = mean.dot(1/setSize)
+
+    #Getting covMatrix
+    sum = np.zeros((pixX,pixY))
+    for i in range(setSize):
+        name = 'train/cat.' + str(i) + '.jpg'
+        img = Image.open(name).convert('L')
+        img = img.resize((pixX,pixY))
+        data = np.asarray(img)
+        data = np.subtract(data, mean)
+        sum = np.add(sum, data.dot(data.transpose()))
+    covMatrix = sum.dot(1/setSize)
+
+    #Eigen value stuff
     eigen = np.linalg.eig(covMatrix)
     sorted = np.sort(eigen[0])
     index_arr = [np.where(eigen[0] == sorted[i]) for i in range(len(eigen[0]))]
+
+    #Get sorted eigen vectors
     new = swap_indexes(eigen[1], index_arr)
     new = np.array(new)
     new = new.dot(255).astype('uint8')
     img = Image.fromarray(new)
-    img.convert("L")
+    img.convert('L')
     img.show()
+
     return 0
 
 def swap_indexes(arr, indexes):
