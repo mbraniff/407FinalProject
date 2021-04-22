b651fa03e2efbf64d667c124cba4584aea6847c8
mbraniff <mbraniff@rocketmail.com>
2021-04-22 17:34:03 -0500

    WOW there are cats

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from PIL import Image
 pixX = 250
 pixY = 250
 
-setSize = 100
+setSize = 1000
 
 def img_mean(img, mean):
     numpydata = np.asarray(img)
+    numpydata = numpydata.ravel()
     mean = np.add(mean, numpydata)
     return mean
 
 
 def main():
-    mean = np.zeros((pixX,pixY))
+    mean = np.zeros((pixX*pixY,))
 
     #Getting mean
     for i in range(setSize):
@@ -24,27 +25,33 @@ def main():
     mean = mean.dot(1/setSize)
 
     #Getting covMatrix
-    sum = np.zeros((pixX,pixY))
+    sum = []
     for i in range(setSize):
         name = 'train/cat.' + str(i) + '.jpg'
         img = Image.open(name).convert('L')
-        img = img.resize((pixX,pixY))
+        img = img.resize((pixX, pixY))
         data = np.asarray(img)
-        data = np.subtract(data, mean)
-        sum = np.add(sum, data.dot(data.transpose()))
-    covMatrix = sum.dot(1/setSize)
+        data = data.ravel()
+        # data = np.subtract(data, mean)
+        sum.append(data)
+    sum = np.array(sum)
+    # sum = sum.dot(1/setSize)
+    sum = sum.transpose()
 
     #Eigen value stuff
-    eigen = np.linalg.eig(covMatrix)
+    L = sum.transpose().dot(sum)
+    eigen = np.linalg.eig(L)
     sorted = np.sort(eigen[0])
     index_arr = [np.where(eigen[0] == sorted[i]) for i in range(len(eigen[0]))]
 
     #Get sorted eigen vectors
     new = swap_indexes(eigen[1], index_arr)
-    new = np.array(new)
-    new = new.dot(255).astype('uint8')
+    new = np.array(new[-1])
+    new = sum.dot(new)
+    new = new.astype('uint8')
+    new = new.reshape((pixX, pixY))
     img = Image.fromarray(new)
-    img.convert('L')
+    img = img.convert('L')
     img.show()
 
     return 0
